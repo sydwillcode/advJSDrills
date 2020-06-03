4aed9b05fa25f8881d3d98756813bb0353a47108
Sydni <will.sydni@gmail.com>
2020-06-03 08:48:32 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+name = "Sydni";
+var name; 
+console.log(name);
+
+// setName();
+// function setName() {
+//     var name = 'Covalence';
+//     console.log(name);
+
+// }
+
+// console.log('text');
+// let avg = findAvg(5,5);
+// console.log('some text', avg);
+// function findAvg (a,b){
+//     console.log('more text');
+//     var answer = (a+b) / 2; 
+//     return answer;
+// }
+
+let fruits = ['banana', 'orange', 'peach'];
+//let favFruit; 
+function printFruit(){
+    //console.log(leastFav);
+    console.log(fruits[0]);
+    let favFruit = fruits[2];
+
+    function showFavFruit(){
+        console.log(favFruit);  
+       // let leastFav = fruits[1];
+    }
+    showFavFruit(); 
+}
+printFruit();
+
+hi();
+function hi(){
+    console.log('Hello, ' + name)
+}
+
+ let example = function(){
+    alert('This is an alert')
+}
+
+example();
+
